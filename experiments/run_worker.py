3ee449cf5387bd8f06a6b4d2226a1cafff80da6f
switham <github@mac-guyver.com>
2013-09-08 20:13:39 -0400

    run_worker.py: gradual output with tty buffering

--- experiments/run_worker.py
+++ experiments/run_worker.py
@@ -3,10 +3,12 @@
 
 from sys import stdin
 import sys
+import os
 import ast
 import traceback
 import StringIO
 import multiprocessing
+import time
 
 
 def get_input_string():
@@ -24,9 +26,84 @@ def get_input_string():
     return "".join(lines)
 
 
-def unixify_newlines(text):
-    return text.replace('\r\n', '\n').replace('\r', '\n')
+class Fd_pipe_wrapper():
+    """
+    Wrapper to redirect an output stream into a multiprocessing.Connection pipe-end,
+    in such a way that it can be multiplexed with other output streams.
+    As in:
+        stdout = Fd_pipe_wrapper(conn, 0)
+        stderr = Fd_pipe_wrapper(conn, 1)
+        print >>stdout, "Output to stdout."
+        print >>stderr, "Message to stderr."
+        conn.send({"eof": True})
+        
+    Uses a convention of sending dictionaries like this through the pipe:
+        {"eof": False, "fd": 0, "str": "Output to stdout.\n"}
+    Each call to a wrapper's write() method produces one of these; in general
+    each is just a chunk of text, not necessarily a whole or single line.
+    Deciding whether and where to put newlines is up to the caller, although
+    print statements (as above) insert them automatically.
+    Decoding where lines end is up to the code at the other end of the pipe.
+    
+    To signal that the fds have closed (all together), code outside this
+    class can send:
+        {"eof": True}
+    This is not sent by the wrappers' close() method, on the theory that
+    there are multple streams to close, but only one eof message should
+    be sent to close them all together.
+    
+    There is no code in this class to interpret these dictionaries on the
+    other end of the pipe.
+    
+    Thanks to ibell at http://stackoverflow.com/questions/11129414
+    """
+    def __init__(self, connection, fd, buffer_size=2048):
+        self.conn = connection
+        self.fd = fd
+        
+    def write(self, string):
+        self.conn.send({"eof": False, "fd": self.fd, "str": string})
+        
+    def flush(self):
+        # This class does no buffering of its own.
+        pass
+    
+    def close(self):
+        # See comments above about eof signalling.
+        pass
 
+
+class Tty_buffer(object):
+    """
+    Do buffering for a file-like object the way the stdout does
+    when connected to a terminal, i.e., flush on newlines.
+    """
+    def __init__(self, file_like_object, buffsize=2048):
+        self.file = file_like_object
+        self.buffsize = buffsize
+        self.buffer = ""
+
+    def write(self, string):
+        self.buffer += string
+        if len(self.buffer) >= self.buffsize:
+            self.flush()
+        else:
+            p = string.rfind('\n')
+            if p > -1:
+                # Flush up to the (last) newline.
+                p = p - len(string) + len(self.buffer)
+                self.file.write(self.buffer[:p + 1])
+                self.file.flush()
+                self.buffer = self.buffer[p + 1:]
+
+    def flush(self):
+        self.file.write(self.buffer)
+        self.file.flush()
+        self.buffer = ""
+
+    def close(self):
+        self.flush()
+    
 
 def be_worker(child_conn):
     worker_globals = {}
@@ -35,18 +112,20 @@ def be_worker(child_conn):
         if not do_run:
             break
 
-        response, trace = interpret(code_string, worker_globals)
-        child_conn.send( (True, response, trace) )
+        stdout = Tty_buffer(Fd_pipe_wrapper(child_conn, 0))
+        stderr = Tty_buffer(Fd_pipe_wrapper(child_conn, 1))
+        interpret(code_string, worker_globals, stdout, stderr)
+        stdout.flush()
+        stderr.flush()
+        child_conn.send({"eof": True})
         
 
-def interpret(code_string, worker_globals):
+def interpret(code_string, worker_globals, stdout, stderr):
     saved_stdout = sys.stdout
     saved_stderr = sys.stderr
-    output = StringIO.StringIO()
-    trace = ""
     try:
-        sys.stdout = output
-        sys.stderr = output
+        sys.stdout = stdout
+        sys.stderr = stderr
         
         tree = ast.parse(code_string, "<your input>")
         code1 = code2 = None
@@ -62,11 +141,27 @@ def interpret(code_string, worker_globals):
         if code2:
             exec code2 in worker_globals
     except Exception, KeyboardInterrupt:
-        trace = traceback.format_exc()
+        print >>sys.stderr, traceback.format_exc()
     finally:
         sys.stdout = saved_stdout
         sys.stderr = saved_stderr
-    return unixify_newlines(output.getvalue()), unixify_newlines(trace)
+
+
+def test_flush():
+    """
+    Test how automatic and forced flushing works for stdout.
+    You can get the worker to run this by saying
+        from run_worker import test_Flush
+        test_flush()
+    """
+    for i in range(10):
+        for j in range(10):
+            print i * 10 + j,
+            time.sleep(.1)
+            if i < 5:
+                sys.stdout.flush()
+        print
+    
 
 
 if __name__ == "__main__":
@@ -74,22 +169,25 @@ if __name__ == "__main__":
     p = multiprocessing.Process(target=be_worker, args=(child_conn,))
     p.start()
 
+    print "PARENT PID =", os.getpid()
+    print "CHILD PID =", p.pid
+    print
     while True:
         input_string = get_input_string()
         if not input_string:
-            parent_conn.send( (False, "", "") )
+            parent_conn.send( (False, "") )
             break
 
         print "-----"
         parent_conn.send( (True, input_string) )
-        alive, response, trace = parent_conn.recv()
-        if response:
-            print response,
-        if trace:
-            print trace,
+        while True:
+            chunk = parent_conn.recv()
+            if chunk["eof"]:
+                break
+            
+            sys.stdout.write(chunk["str"])
+            sys.stdout.flush()
         print "====="
-        if not alive:
-            break
         
     print "Worker quitting..."
     p.join()
